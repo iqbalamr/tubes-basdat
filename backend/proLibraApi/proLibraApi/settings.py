90bd396d3991a2ab08ef4ecbb248887dc60db03d
dhifafaz <dhifaf.az@gmail.com>
2021-05-02 06:21:57 +0700

    new

--- backend/proLibraApi/proLibraApi/settings.py
+++ backend/proLibraApi/proLibraApi/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    #ProLibra application
+    'ProLibra.apps.ProlibraConfig',
+    #CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +52,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
 
 ROOT_URLCONF = 'proLibraApi.urls'
 
