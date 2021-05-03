0663483e184cfb562eff6a95e25939ab66f486c0
dhifafaz <dhifaf.az@gmail.com>
2021-05-03 19:30:04 +0700

    new

--- backend/library/apps.py
+++ backend/library/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProlibraConfig(AppConfig):
+class LibraryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProLibra'
+    name = 'library'
---
+++ backend/library_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import Post
+
+app_name = 'library_api'
+
+urlpatterns = [
+  # path('', TemplateView.as_view(template_name="library/index.html")),
+
+]
