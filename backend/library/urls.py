26474d6e3d1abdb729df952d52f5fdce3300d7f2
dhifafaz <dhifaf.az@gmail.com>
2021-05-03 20:19:40 +0700

    new

--- backend/library/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'library'
-
-urlpatterns = [
-  path('', TemplateView.as_view(template_name="library/index.html")),
-
-]
--- backend/library_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from .views import Post
-
-app_name = 'library_api'
-
-urlpatterns = [
-  # path('', TemplateView.as_view(template_name="library/index.html")),
-
-]
